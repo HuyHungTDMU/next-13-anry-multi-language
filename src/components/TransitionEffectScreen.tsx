924e4065f68a9e42d37dfe7c6a6df2d3b67c04d4
Huy Hùng <huyhung.tdm@gmail.com>
2023-12-05 18:30:53 +0700

    chore: update TransitionEffectScreen

--- src/components/TransitionEffectScreen.tsx
+++ src/components/TransitionEffectScreen.tsx
@@ -19,7 +19,7 @@ const TransitionEffectScreen = () => {
         transition={{ delay: 0.2, duration: 0.8, ease: "easeInOut" }}
       />
       <motion.div
-        className="fixed top-0 bottom-0 right-full w-screen h-screen z-10 bg-[#667f7b]"
+        className="fixed top-0 bottom-0 right-full w-screen h-screen z-10 bg-[#031E2F]"
         initial={{ x: "100%", width: "100%" }}
         animate={{ x: "0%", width: "100%" }}
         transition={{ delay: 0.4, duration: 0.8, ease: "easeInOut" }}
